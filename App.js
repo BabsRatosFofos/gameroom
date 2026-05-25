c47be3db1e06b69b1a6572881268602c82696c01
Aluno.Etec <aula.etec.com>
2026-05-25 16:29:00 -0300

    commit

--- App.js
+++ App.js
@@ -1,52 +1,152 @@
-import { StyleSheet,Text, View, TextInput, TouthableOpacity } from 'react-native';
+import { StyleSheet, Text, View, 
+    TextInput, TouchableOpacity } from 'react-native';
 import { useState, useEffect } from 'react';
-import { io } from 'socket.io-client';
+import { io } from 'socket.io-client'; 
 
-socket = io('https://squarewebsocketbackend.onrender.com', {
+const socket = io('https://squarewebsocketbackend.onrender.com', {
   transports: ['websocket']
 });
 
 export default function App() {
 
   const [nome, setNome] = useState('');
-  const [entrou, setEntrou] = useState('false');
+  const [entrou, setEntrou] = useState(false);
   const [usuarios, setUsuarios] = useState([]);
   const [id, setId] = useState(null);
 
   useEffect(() => {
     socket.on('player', (id) => {
       setId(id);
-    }):
+    });
 
     socket.on('update', (users) => {
       setUsuarios(users);
-    }):
+    });
 
     return () => {
       socket.off('player');
-      socket.of('update');
+      socket.off('update');
     }
-    
   }, []);
-  if(entrou == false){
+
+  function entrouGameRoom() {
+    if (!nome) {
+      return;
+    }
+
+    socket.emit("join", nome);
+    setEntrou(true);
+  }
+
+  function movimentar(dx, dy) {
+    const usuario = usuarios.find(user => user.id == id);
+
+    if (!usuario) {
+      console.log("Usuário não encontrado!");
+      return;
+    }
+
+    socket.emit("move", {
+      x: usuario.x + dx,
+      y: usuario.y + dy
+    })
+  }
+
+  if (entrou == false) {
     return (
-      <View>
-        <text>Digite seu Nome</text>
-        <TextInput>
+      <View style={styles.centralizar}>
+        <Text>Digite seu nick:</Text>
+        <TextInput
+          style={styles.input}
           value={nome}
-          onChangeText={(novotexto) => setNome(nomeTexto)}
+          onChangeText={(novoTexto) => setNome(novoTexto)}
+        >
         </TextInput>
-        <TouchableOpacity>
-        <text>Entrar</text>
+        <TouchableOpacity onPress={() => entrouGameRoom()}>
+          <Text>Entrar</Text>
         </TouchableOpacity>
       </View>
-  ):
+    );
+  }
+  else {
+    return (
+      <View style={styles.areaGlobal}>
+        <View style={styles.areaJogo}>  
+          {
+            usuarios.map(usuario => (
+              <View
+                style={[
+                  styles.player,
+                  {
+                    left: usuario.x,
+                    top: usuario.y,
+                    backgroundColor: 
+                      usuario.id == id ? "green" : "blue"
+                  }
+                ]}
+              >
+                <Text>{usuario.name}</Text>
+              </View>
+            ))
+          }
+        </View>
+
+
+        <View style={styles.controles}>
+          <TouchableOpacity onPress={() => movimentar(0, -20)} >
+            <Text>Cima</Text>
+          </TouchableOpacity>
+
+          <View style={styles.esquerdaDireita}>
+            <TouchableOpacity onPress={() => movimentar(-20, 0)}>
+              <Text>Esquerda</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => movimentar(20, 0)}>
+              <Text>Direita</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity onPress={() => movimentar(0, 20)}>
+              <Text>Baixo</Text>
+            </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
-else{
-  return (
-    <View>
-    </View>
-  );
-}
-}
+const styles = StyleSheet.create({
+  centralizar: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  input: {
+    borderWidth: 1,
+    width: 200,
+    margin: 10,
+    padding: 5
+  },
+  areaGlobal: {
+    flex: 1
+  },
+  areaJogo: {
+    flex: 1,
+    backgroundColor: "#EEEEEE"
+  },
+  player: {
+    position: "absolute",
+    width: 60,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  controles: {
+    alignItems: "center",
+    padding: 20
+  },
+  esquerdaDireita: {
+    flexDirection: "row",
+    width: 200,
+    justifyContent: "space-between"
+  }
+});
